c98058f9a68f816ff6d0567826d1e6139dc191d9
Giuseppe Cuccu <giuseppe.cuccu@gmail.com>
2018-03-06 15:21:56 +0100

    Added basic neural networks framework

--- lib/machine_learning_workbench.rb
+++ lib/machine_learning_workbench.rb
@@ -2,6 +2,8 @@ require 'nmatrix'
 
 module MachineLearningWorkbench
   module Compression
+  module NeuralNetwork
+  end
   end
   module Tools
   end
@@ -10,3 +12,4 @@ end
 require_relative 'machine_learning_workbench/monkey'
 require_relative 'machine_learning_workbench/tools'
 require_relative 'machine_learning_workbench/compression'
+require_relative 'machine_learning_workbench/neural_network'
