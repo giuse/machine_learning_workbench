f66d170033883faf16fcdbe008273962084ffb0d
Giuseppe Cuccu <giuseppe.cuccu@gmail.com>
2018-04-06 09:05:42 +0200

    Fixes #1
    Updated this example to Numo.

--- examples/neuroevolution.rb
+++ examples/neuroevolution.rb
@@ -8,13 +8,13 @@ FFNN = WB::NeuralNetwork::FeedForward
 
 # Let's address the XOR problem, as it requires nonlinear fitting
 XOR = {[0,0] => 0, [1,0] => 1, [0,1] => 1, [1,1] => 0}
-# A classic [2,2,1] feed-forward network will do: 2 inputs, 2 hidden, 1 output
-# For other uses, make sure you match the first number to the number of inputs, and
-# the last one as the number of outputs; then add as many layers as needed, by
-# specifying the size of each. Here we have only one, of size 2.
-# NOTE: If this totals thousands of weights, you may want to switch to SNES or BDNES
-# for speed. In the second case, use the function `nweights_per_layer` when instantiating
-# BDNES rather than `nweights`.
+# A classic [2,2,1] (2 inputs, 2 hidden neurons, 1 output neurons) feed-forward
+# network with nonlinear activations can solve this problem.
+# To approximate more complex functions, keep the number of inputs and outputs
+# fixed (they depend on the problem) and increase the number and/or size of
+# hidden neurons. For example: [2, 10, 7, 4, 1].
+# NOTE: If your network grows above few thousands of weights, XNES may be too slow.
+# Try using SNES for large shallow networks or BDNES for deep networks.
 NET = FFNN.new [2,2,1], act_fn: :logistic
 # Note: the process is exactly the same, from instantiation to training, for recurrent
 # networks using the class `WB::NeuralNetwork::Recursive`.
@@ -35,8 +35,8 @@ def fitness weights
   # - observation: correct value, our target
   pred_obs = XOR.map do |input, obs|
     # The network can have an arbitrary number of output neurons
-    # Since here we have only one, we extract the value calling `#first`
-    output = NET.activate(input).first
+    # Since here we have only one, we extract the value as the output
+    output = NET.activate(input)[0]
     # Here we interpret the output as classification
     pred = output > 0.5 ? 1 : 0
     # Finally accumulate prediction-observation pairs
@@ -50,9 +50,12 @@ end
 # Next comes initializing the black-box stochastic optimization algorithm
 # We are searching for the network's weights, this gives us the search space dimensionality
 # We'll use XNES as we are working with less than 100 dimensions (weights)
-nes = XNES.new NET.nweights, method(:fitness), :max, rseed: 15
-# Note: the random seed is fixed here to ensure the task is solved in one try in few iterations
-# In a real task, best using an over-large network, more iterations, and try several seeds
+nes = XNES.new NET.nweights, method(:fitness), :max, rseed: 0
+# Note BDNES requires `NET.nweights_per_layer` rather than `NET.nweights` in initialization:
+# nes = WB::Optimizer::NaturalEvolutionStrategies::BDNES.new NET.nweights_per_layer,
+#   method(:fitness), :max, rseed: 10
+# The random seed is fixed here to ensure a reproducible behavior
+# In a real task, best using an oversized network, more iterations, and try several seeds
 
 # NOTE: In practical applications it is best to delegate parallelization to the fitness
 # function instead of computing the fitness of one individual at a time. This can be
@@ -60,16 +63,20 @@ nes = XNES.new NET.nweights, method(:fitness), :max, rseed: 15
 # setting the `parallel_fit` switch to `true`:
 # nes = XNES.new NET.nweights,
 #   -> (genotypes) { Parallel.map genotypes, &method(:fitness) },
-#   :max, rseed: 15, parallel_fit: true
+#   :max, rseed: 0, parallel_fit: true
 
 
-# Nothing left but to run the optimization algorithm, few epochs here will suffice
+# Nothing left but to run the optimization algorithm
+# Depending on the random seed (read: luck)few epochs here will suffice
 50.times { nes.train }
 # OK! now remember, `NET` currently holds the weights of the last evaluation
 # Let's fetch the best individual found so far
 best_fit, best_weights = nes.best
 # Let's run them again to check they work
-result = fitness best_weights # careful here if you defined a parallel `fitness`
-puts "The found network achieves a score of #{result} out of 4 in the XOR task"
-puts "Weights: #{best_weights}"
+result = fitness best_weights
+# Note if you defined a parallel fitness above you'll need instead
+# result = fitness([best_weights])[0]
+puts "The found network achieves a score of #{result} out of #{XOR.size} in the XOR task"
+puts "Weights: #{best_weights.to_a}"
 puts "Done!"
+# That's it! 18 lines and you got a working neuroevolution algorithm, congrats :)
